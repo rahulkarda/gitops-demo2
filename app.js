cae7e4cd3a093581d119836b3905d87621dde1e2
Rahul Karda <rahul.karda@sap.com>
2025-02-16 16:24:09 +0530

    Updated app message and image version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, GitOps with ArgoCD & FluxCD!");
+  res.send("Hello, GitOps with ArgoCD & FluxCD!!!!!!!!!!!!!!!!!!!!!!!!");
 });
 
 app.listen(PORT, () => {
