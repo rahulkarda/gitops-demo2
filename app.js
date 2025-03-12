2750b783073b41253fde2208c89009a4f4e1fefd
Rahul Karda <rahul.karda@sap.com>
2025-03-12 11:13:28 +0530

    changes in html

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files (CSS)
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("Hello, GitOps with ArgoCD & FluxCD!!!!!!!!!!!!!!!!!!!!!!!!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
